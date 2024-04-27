ff67b7a37dc71b14a84bcf34449197f87c0b7bd2
prodini03 <luischafariz@gmail.com>
2024-04-27 18:39:15 +0100

    Models adicionados

---
+++ palavrao/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db import models
+from django.utils import timezone
+import datetime
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Client(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+class Puzzle(models.Model):
+    word = models.CharField(max_length=5)
+
+class Tentativa(models.Model):
+    puzzle = models.ForeignKey(Puzzle,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    tentativa = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    user = models.ForeignKey(Client, on_delete=models.CASCADE)
+    puzzle_id = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
