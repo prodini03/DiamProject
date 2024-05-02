5cb016ae7bba605322a6f64f7c00323de750458f
DPalhas <dpnps@iscte-iul.pt>
2024-05-02 20:16:03 +0100

    scripts

---
+++ palavrao/static/script.js
@@ -0,0 +1,27 @@
+import { PALAVRAS } from "./palavroes.js";
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = PALAVRAS[Math.floor(Math.random() * PALAVRAS.length)]
+console.log(rightGuessString)
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
